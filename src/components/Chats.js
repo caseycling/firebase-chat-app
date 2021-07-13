f2d2852de37a9cd5422d31cb931b02cfaaa52ad1
Casey <caseyclinga@gmail.com>
2021-07-13 10:27:05 -0700

    Chats page component with logout functionality

---
+++ src/components/Chats.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import { auth } from '../firebase';
+
+import { useAuth } from '../contexts/AuthContext';
+
+const Chats = () => {
+  const history = useHistory();
+  const { user } = useAuth();
+
+  console.log(user);
+
+  const handleLogout = async () => {
+    await auth.signOut();
+
+    history.push('/');
+  };
+
+  return (
+    <div className='chats-page'>
+      <div className='nav-bar'>
+        <div className='logo-tab'>Unichat</div>
+        <div className='logout-tab' onClick={handleLogout}>
+          Logout
+        </div>
+      </div>
+
+      <ChatEngine
+        height='calc(100vh - 66px)'
+        projectId='1ffc37d3-b3cb-4cd8-b939-ff904dc8f4c7'
+        userName='.'
+        userSecret='.'
+      />
+    </div>
+  );
+};
+
+export default Chats;
