4418a3c083d7d8086526af21303a235f425564b2
Casey <caseyclinga@gmail.com>
2021-07-14 10:42:21 -0700

    Added environment variables

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { ChatEngine } from 'react-chat-engine';
 import { auth } from '../firebase';
 
 import { useAuth } from '../contexts/AuthContext';
+import axios from 'axios';
 
 const Chats = () => {
   const history = useHistory();
   const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
 
   console.log(user);
 
@@ -17,6 +19,53 @@ const Chats = () => {
     history.push('/');
   };
 
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], 'userPhoto.jpg', { type: 'image/jpeg' });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      history.push('/');
+      return;
+    }
+    //Get user from chatengine.io
+    axios
+      .get('https://api.chatengine.io/users/me/', {
+        headers: {
+          'project-id': process.env.REACT_APP_CHAT_ENGINE_ID,
+          'user-name': user.email,
+          'user-secret': user.uid,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setLoading(false);
+      })
+      //If no user exists, create new one
+      .catch(() => {
+        let formdata = new FormData();
+        formdata.append('email', user.email);
+        formdata.append('username', user.email);
+        formdata.append('secret', user.uid);
+
+        getFile(user.photoURL).then((avatar) => {
+          formdata.append('avatar', avatar, avatar.name);
+          axios
+            .post('https://api.chatengine.io/users/', formdata, {
+              headers: {
+                'private-key': process.env.REACT_APP_CHAT_ENGINE_KEY,
+              },
+            })
+            .then(() => setLoading(false))
+            .catch((error) => console.log(error));
+        });
+      });
+  }, [user, history]);
+
+  if (!user || loading) return 'Loading...';
   return (
     <div className='chats-page'>
       <div className='nav-bar'>
@@ -28,9 +77,9 @@ const Chats = () => {
 
       <ChatEngine
         height='calc(100vh - 66px)'
-        projectId='1ffc37d3-b3cb-4cd8-b939-ff904dc8f4c7'
-        userName='.'
-        userSecret='.'
+        projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
+        userName={user.email}
+        userSecret={user.uid}
       />
     </div>
   );
